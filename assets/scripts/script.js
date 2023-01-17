fea0c37395861d163399db430101e31ea03fea00
Jonathan Davis <trunten@btinternet.com>
2023-01-17 16:16:34 +0000

    Added an alert if an empty string was entered

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,91 +1,22 @@
 // Array of special characters to be included in password
 const specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
-  "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
+  "@",  "%",  "+",  "\\",  "/",  "'",  "!",  "#",  "$",  "^",  "?", 
+   ":",  ",",  ")", "(",  "}",  "{",  "]",  "[",  "~",  "-",  "_",  ".",
 ];
 
 // Array of numeric characters to be included in password
-const numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 // Array of lowercase characters to be included in password
 const lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
+  "a",  "b",  "c",  "d",  "e",  "f",  "g",  "h",  "i",  "j",  "k",  "l",  "m",  
+  "n",  "o",  "p",  "q",  "r",  "s",  "t",  "u",  "v",  "w",  "x",  "y",  "z",
 ];
 
 // Array of uppercase characters to be included in password
 const upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
+  "A",  "B",  "C",  "D",  "E",  "F",  "G",  "H",  "I",  "J",  "K",  "L",  "M",
+  "N",  "O",  "P",  "Q",  "R",  "S",  "T",  "U",  "V",  "W",  "X",  "Y",  "Z",
 ];
 
 // Create an object to hold all my character sets for additional clarity.
@@ -121,7 +52,7 @@ function getPasswordOptions() {
 
   // First prompt the user for a password length
   let length = prompt("How long do you want you password to be? (At least 10 characters and no more than 64");
-  if (!length) { return; }
+  if (length === null) { return; } // Null means user pressed cancel.
 
   // Convert supplied  user value into an integer
   length = parseInt(length);
